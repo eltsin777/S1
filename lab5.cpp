43d12bbe6e180f0982e1462f8c5434d9e0779add
Eltsin Andrey <flammable666@mail.ru>
2024-11-12 17:53:38 +0500

    добавил функцию деления

--- lab5.cpp
+++ lab5.cpp
@@ -13,6 +13,7 @@ int main()
     cout << x << "+" << y << "=" << x + y << endl;
     cout << x << "-" << y << "=" << x - y << endl;
     cout << x << "*" << y << "=" << x * y << endl;
+    cout << x << "/" << y << "=" << x / y << endl;
     return 0;
 }
 
